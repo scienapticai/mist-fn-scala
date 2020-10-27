cc70d38f98ca61f670a20d19cbcb412ad3ba382d
Mithun Raj Arackal <mithun@scienaptic.com>
2020-10-27 15:25:57 +0530

    Parquet to delta conversion for scored files in lens

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "sample-app"
+name := "delta-conversion"
 
 version := "0.0.1"
 
@@ -6,11 +6,17 @@ scalaVersion := "2.12.11"
 
 val sparkVersion = "2.4.2"
 
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+
 libraryDependencies ++= Seq(
   "io.hydrosphere" %% "mist-lib" % "1.1.1",
 
   "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
   "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
   "org.apache.spark" %% "spark-hive" % sparkVersion % "provided",
-  "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided"
+  "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided",
+  "io.delta" %% "delta-core" % "0.6.0"
 )
--- src/main/scala/com/scienaptic/Application.scala
+++ src/main/scala/com/scienaptic/Application.scala
@@ -3,24 +3,75 @@ package com.scienaptic
 import mist.api._
 import mist.api.dsl._
 import mist.api.encoding.defaults._
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileStatus, FileSystem, LocalFileSystem, Path}
+import org.apache.hadoop.hdfs.DistributedFileSystem
 import org.apache.spark.sql.SparkSession
 
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
+
+/**
+ * Application to convert parquet files within a specific folder pattern to delta format
+ */
 object Application extends MistFn with Logging {
 
   override def handle: Handle = {
     withArgs(
-      arg[String]("inputPath")
+      arg[String]("host", "localhost"), arg[Int]("port", 9000), arg[String]("filesPath", "/data")
     )
-      .onSparkSession { (path: String, spark: SparkSession) => {
-        val filePath = s"file://$path"
-        val df = spark.read
-          .format("csv")
-          .option("header", value = true)
-          .load(filePath)
-        val fileName = filePath.split("\\.").dropRight(1).mkString(",")
-        val outputPath = s"$fileName.parquet"
-        df.write.mode("overwrite").parquet(outputPath)
-        outputPath
+      .onSparkSession { (host: String, port: Int, filesPath: String, spark: SparkSession) => {
+        val defaultFs = s"hdfs://$host:$port"
+
+        val conf: Configuration = new Configuration()
+
+        // File system implementations.
+        conf.set("fs.hdfs.impl", classOf[DistributedFileSystem].getName)
+        conf.set("fs.file.impl", classOf[LocalFileSystem].getName)
+        // nameNode host and port
+        conf.set("fs.defaultFS", defaultFs)
+
+        //  conf.addResource(new Path("file://" + "/usr/local/hadoop/etc/hadoop" + "/core-site.xml")); // Replace with actual path
+        //  conf.addResource(new Path("file://" + "/usr/local/hadoop/etc/hadoop" + "/hdfs-site.xml")); // Replace with actual path
+
+        conf.setBoolean("dfs.support.append", true)
+
+        val fileSystem: FileSystem = FileSystem.get(conf)
+
+        import scala.util.matching.Regex
+        val pattern: Regex = "/data/[0-9]+/[0-9]+/scores/[0-9]+".r
+
+        @tailrec
+        def rec(array: List[FileStatus], list: List[String]): List[String] = {
+          array match {
+            case Nil => list
+            case ::(head, tl) =>
+              if (pattern.findFirstIn(head.getPath.toString).isDefined) {
+                rec(tl, head.getPath.toString :: list)
+              } else if (head.isDirectory) {
+                rec(tl ++ fileSystem.listStatus(head.getPath).toList, list)
+              } else {
+                rec(tl, list)
+              }
+          }
+        }
+        val filePaths = rec(fileSystem.listStatus(new Path(filesPath)).toList, List.empty[String])
+        val totalFiles = filePaths.length
+
+        import io.delta.tables._
+        filePaths.foldLeft(0) { (acc, path) =>
+          logger.info(s"converting file number $acc out of $totalFiles to delta: $path")
+
+          // Convert un-partitioned parquet table at specified path
+          Try(DeltaTable.convertToDelta(spark, s"parquet.`$path`")) match {
+            case Failure(exception) =>
+              logger.error(s"Error converting to delta format: $path")
+              logger.error(exception.getMessage)
+            case Success(_) => ()
+          }
+          acc + 1
+
+        }
       }
       }
   }.asHandle
