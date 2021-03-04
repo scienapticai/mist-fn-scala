7fd5637b2076b4f404a11874d27e1b78d20232ea
Apoorv Shrivastava <apoorv@scienaptic.com>
2021-03-04 20:40:48 +0530

    Refactor

--- src/main/scala/com/scienaptic/Application.scala
+++ src/main/scala/com/scienaptic/Application.scala
@@ -3,6 +3,7 @@ package com.scienaptic
 import mist.api._
 import mist.api.dsl._
 import mist.api.encoding.defaults._
+
 import org.apache.spark.sql.SparkSession
 
 object Application extends MistFn with Logging {
@@ -10,19 +11,18 @@ object Application extends MistFn with Logging {
   override def handle: Handle = {
     withArgs(
       arg[String]("inputPath")
-    )
-      .onSparkSession { (path: String, spark: SparkSession) => {
+    ).onSparkSession { (path: String, spark: SparkSession) =>
+      {
         val filePath = s"file://$path"
         val df = spark.read
-          .format("csv")
           .option("header", value = true)
-          .load(filePath)
+          .csv(filePath)
         val fileName = filePath.split("\\.").dropRight(1).mkString(",")
         val outputPath = s"$fileName.parquet"
         df.write.mode("overwrite").parquet(outputPath)
         outputPath
       }
-      }
+    }
   }.asHandle
 
 }
